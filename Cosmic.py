843b65ef2697b6eb874c75ed187d7e620e9a6c73
Aashishsha <77851822+Aashishsha@users.noreply.github.com>
2021-01-30 15:18:52 +0530

    Update Cosmic.py

--- Cosmic.py
+++ Cosmic.py
@@ -3,7 +3,7 @@ from tkinter import *
 root = Tk ()
 root.geometry('300x200')
 root.title("new")
-data = Label (text = "Dear Aashish shaw")
+data = Label (text = "Dear Aashish shaw").
 data.pack()
 
 #welcome
